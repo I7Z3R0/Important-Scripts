bd5ae309e18b211e455effd3f0bb670337db040e
Imran Thoufeeque <75982271+I7Z3R0@users.noreply.github.com>
2023-08-19 19:17:34 +0530

    Create ready.sh

---
+++ Ready_machines/ready.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Enter the target IP Address : "
+read target_ip
+mkdir -p nmap www exploit research
+if [ ! -e notes.md ]; then
+    touch notes.md
+    ip_tun0=$(sh -c 'ip a | awk "/tun0\$/{gsub(/\/.*/, \"\"); print \$2}"')
+    echo -e "MY IP = $ip_tun0" >> notes.md
+    echo -e "Target = $target_ip" >> notes.md
+    subl notes.md
+fi
